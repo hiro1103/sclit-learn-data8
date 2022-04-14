d139f90e2a23c82a6ab28b3af9a94456592a121e
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-15 05:21:11 +0900

    Add files via upload

--- data8.py
+++ data8.py
@@ -1,7 +1,24 @@
+
 import pyprind
 import pandas as pd
 import os
 import numpy as np
+import re
+import nltk
+import pickle
+from matplotlib.pyplot import clf
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import CountVectorizer
 
 basepath = 'C:\\Users\\murak\\python\\aclImdb'
 labels = {'pos': 1, 'neg': 0}
@@ -11,15 +28,177 @@ for s in ('test', 'train'):
     for l in ('pos', 'neg'):
         path = os.path.join(basepath, s, l)
         for file in sorted(os.listdir(path)):
-            with open(os.path.join(path, file), 'r', encoding='utf-8') as infile:
+            with open(os.path.join(path, file),
+                      'r', encoding='utf-8') as infile:
                 txt = infile.read()
-            df = df.append([[txt, labels[l]]], ignore_index=True)
+            df = df.append([[txt, labels[l]]],
+                           ignore_index=True)
             pbar.update()
-
 df.columns = ['review', 'sentiment']
+
 np.random.seed(0)
 df = df.reindex(np.random.permutation(df.index))
 df.to_csv('movie_data.csv', index=False, encoding='utf-8')
 df = pd.read_csv('movie_data.csv', encoding='utf-8')
 print(df.head(3))
 print(df.shape)
+
+count = CountVectorizer()
+docs = np.array([
+    'The sun is shining',
+    'The weather is sweet',
+    'The sun is shining, the weather is sweet, and one and one is two'])
+bag = count.fit_transform(docs)
+print(count.vocabulary_)
+print(bag.toarray())
+tfidf = TfidfTransformer(use_idf=True, norm='l2', smooth_idf=True)
+np.set_printoptions(precision=2)
+print(tfidf.fit_transform(count.transform(docs)).toarray())
+print(df.loc[0, 'review'][-50:])
+
+
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',
+                           text)
+    text = (re.sub('[\W]+', ' ', text.lower()) +
+            ' '.join(emoticons).replace('-', ''))
+    return text
+
+
+print(preprocessor(df.loc[0, 'review'][-50:]))
+print(preprocessor("</a>This :) is :(a test :-)!"))
+df['review'] = df['review'].apply(preprocessor)
+
+
+def tokenizer(text):
+    return text.split()
+
+
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
+
+
+print(tokenizer('runners like running and thus they run'))
+porter = PorterStemmer()
+
+
+tokenizer_porter('runners like running and thus they run')
+nltk.download('stopwords')
+stop = stopwords.words('english')
+[w for w in tokenizer_porter('a runner likes running and runs a lot')[-10:]
+ if w not in stop]
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+
+tfidf = TfidfVectorizer(strip_accents=None,
+                        lowercase=False,
+                        preprocessor=None)
+
+param_grid = [{'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              {'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'vect__use_idf':[False],
+               'vect__norm':[None],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              ]
+
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(random_state=0, solver='liblinear'))])
+
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                           scoring='accuracy',
+                           cv=5,
+                           verbose=2,
+                           n_jobs=-1)
+
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) + \
+        ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+
+def stream_docs(path):
+    with open(path, 'r', encoding='utf-8') as csv:
+        next(csv)  # skip header
+        for line in csv:
+            text, label = line[:-3], int(line[-2])
+            yield text, label
+
+
+print(next(stream_docs(path='movie_data.csv')))
+
+
+def get_minibatch(doc_stream, size):
+    docs, y = [], []
+    try:
+        for _ in range(size):
+            text, label = next(doc_stream)
+            docs.append(text)
+            y.append(label)
+    except StopIteration:
+        return None, None
+    return docs, y
+
+
+vect = HashingVectorizer(decode_error='ignore',
+                         n_features=2*21,
+                         preprocessor=None,
+                         tokenizer=tokenizer)
+
+clf = SGDClassifier(loss='log', random_state=1)
+doc_stream = stream_docs(path='movie_data.csv')
+
+pbar = pyprind.ProgBar(45)
+classes = np.array([0, 1])
+for _ in range(45):
+    X_train, y_train = get_minibatch(doc_stream, size=1000)
+    if not X_train:
+        break
+    X_train = vect.transform(X_train)
+    clf.partial_fit(X_train, y_train, classes=classes)
+    pbar.update()
+
+X_test, y_test = get_minibatch(doc_stream, size=5000)
+X_test = vect.transform(X_test)
+print('Accuracy: %.3f' % clf.score(X_test, y_test))
+
+clf = clf.partial_fit(X_test, y_test)
+
+df = pd.read_csv('movie_data.csv', encoding='utf-8')
+count = CountVectorizer(stop_words='english',
+                        max_df=0.1,
+                        max_features=5000)
+X = count.fit_transform(df['review'].values)
+lda = LatentDirichletAllocation(n_components=10,
+                                random_state=123,
+                                learning_method='batch')
+X_topics = lda.fit_transform(X)
+
+print(lda.components_.shape)
+
+n_top_words = 5
+feature_names = count.get_feature_names()
+for topic_idx, topic in enumerate(lda.components_):
+    print("Topic %d:" % (topic_idx + 1))
+    print(" ".join([feature_names[i]
+                    for i in topic.argsort()
+                    [:-n_top_words-1:-1]]))
+
+
+horror = X_topics[:, 5].argsort()[::-1]
+for iter_idx, movie_idx in enumerate(horror[:3]):
+    print('\nnHorror movie #%d:' % (iter_idx+1))
+    print(df['review'][movie_idx][:300], '...')
